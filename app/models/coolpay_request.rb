d763654a5da988ff8beebea4bd54e66ee7b06240
emmpak <eiliev05@yahoo.com>
2017-06-24 10:14:47 +0100

    Add header parameter to set_post_request method

--- app/models/coolpay_request.rb
+++ app/models/coolpay_request.rb
@@ -1,12 +1,20 @@
 class CoolpayRequest
-    attr_reader :uri
+    attr_reader :uri, :http, :header
 
     def initialize(uri)
       @uri = URI.parse(uri)
       @header = {'Content-Type': 'application/json'}
+      @http = set_http_connection
     end
 
-    def set_http
+    def set_post_request(body, header)
+      request = Net::HTTP::Post.new(uri.request_uri, header)
+      request.body = body.to_json
+      request
+    end
+
+    private
+    def set_http_connection
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http
--- spec/models/coolpay_request_spec.rb
+++ spec/models/coolpay_request_spec.rb
@@ -3,6 +3,8 @@ require './app/models/coolpay_request'
 
 describe CoolpayRequest do
   let(:uri) { "https://www.google.com" }
+  let(:header) { {'Content-Type': 'application/json'} }
+  let(:credentials) { {"username": "your_username","apikey": "5up3r$ecretKey!"} }
   subject(:request) { described_class.new(uri) }
 
 
@@ -12,9 +14,27 @@ describe CoolpayRequest do
     end
   end
 
-  describe('#set_http') do
+  describe('#http') do
     it('creates an http objet') do
-      expect(request.set_http).to be_kind_of(Net::HTTP)
+      expect(request.http).to be_kind_of(Net::HTTP)
+    end
+  end
+
+  describe('#set_post_request') do
+    before do
+      @http_request = request.set_post_request(credentials, header)
+    end
+
+    it('creates a post request') do
+      expect(@http_request).to be_kind_of(Net::HTTP::Post)
+    end
+
+    it('has stores the given message in the body of the request') do
+      expect(@http_request.body).to eq(credentials.to_json)
+    end
+
+    it('has the given header') do
+      expect(@http_request['content-type'.to_sym]).to eq 'application/json'
     end
   end
 end
