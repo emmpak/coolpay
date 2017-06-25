65b2acadf8f6679ae3408a3cc953cec0f128502c
emmpak <eiliev05@yahoo.com>
2017-06-25 17:12:58 +0100

    Add payment form

--- app/app.rb
+++ app/app.rb
@@ -40,7 +40,7 @@ class Coolpay < Sinatra::Base
     erb :'payments/index'
   end
 
-  post '/recipients/new' do
+  post '/recipients' do
     client = HTTPClient.new(uri: "https://coolpay.herokuapp.com/api/recipients")
     request = HTTPRequest.new(type: 'POST', uri: client.get_request_uri)
     response = client.http.request(request.build(message: {'recipient': {'name': params[:recipient]}}, token: token))
