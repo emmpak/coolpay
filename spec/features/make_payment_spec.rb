4e43355def20d080114bc2caf29cb82056a62698
emmpak <eiliev05@yahoo.com>
2017-06-26 14:16:01 +0100

    Fix feature test for making a payment

--- spec/features/make_payment_spec.rb
+++ spec/features/make_payment_spec.rb
@@ -3,11 +3,11 @@ feature 'Make a payment' do
     visit('/')
     click_button('Make a payment')
     within '#paymentform-1' do
-      fill_in :amount, with: 100
+      fill_in :amount, with: 1000
       select 'GBP', from: 'currency'
       click_button "Send coins"
     end
     expect(current_path).to eq '/payments'
-    expect(page).to have_content "You successfuly transferred coins."
+    expect(page).to have_content "1000 GBP"
   end
 end
