dd5afc778254a6aeb37e9c516282263b2a59135b
emmpak <eiliev05@yahoo.com>
2017-06-26 13:40:45 +0100

    Add further styling on payments page

--- app/controllers/recipients.rb
+++ app/controllers/recipients.rb
@@ -1,7 +1,7 @@
 class Coolpay < Sinatra::Base
 
   post '/recipients' do
-    api.send_request(path: 'recipients', type: 'POST', token: token)
+    api.send_request(path: 'recipients', type: 'POST', token: token, message: {'recipient': {'name': params[:recipient]}})
     redirect to '/payments'
   end
 end
